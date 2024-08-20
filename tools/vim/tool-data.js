c7548e6fcc003448864b3655c0d1546ad1d73bfd
Felix Hohn <felix.hohn@maibornwolff.de>
2024-08-20 22:37:56 +0200

    revert 9beeb6cdd197555e9898131ea701a32ddeab7460

--- tools/vim/tool-data.js
+++ tools/vim/tool-data.js
@@ -1,13 +1,35 @@
 const locationFile = window.location.href.split('/').pop();
 const extractedTool = locationFile.split('.')[0];
 
-fetch(`?./${extractedTool}.json`)
-.then((response) => response.json())
-.then((data) => {
-  const keys = document.querySelectorAll("keyboard-key");
+const Tool = {
+  VIM: "vim",
+  ARC: "arc"
+}
 
-  for (const key of keys) {
-    if (key.id in data) key.setAttribute("data-shortcuts", JSON.stringify(data[key.id]));
-  }
-})
-.catch((error) => console.error(error));
+switch (extractedTool) {
+  case Tool.VIM:
+    fetchVIM();
+    break;
+  case Tool.ARC:
+    fetchARC();
+    break;
+  default:
+    break;
+}
+
+function fetchVIM() {
+  fetch("./vim.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const keys = document.querySelectorAll("keyboard-key");
+
+    for (const key of keys) {
+      if (key.id in data) key.setAttribute("data-shortcuts", JSON.stringify(data[key.id]));
+    }
+  })
+  .catch((error) => console.error(error));
+}
+
+function fetchARC() {
+  console.log("Get Arc shortcuts..");
+}
